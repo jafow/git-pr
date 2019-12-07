cee6910ee32f718de3d8b0605a925c31ed6978a0
jafow <jared.a.fowler@gmail.com>
2019-12-06 20:26:54 -0800

    noop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
-#[runtime::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    let mut res = surf::get("https://httpbin.org/get").await?;
-    dbg!(res.body_string().await?);
-    Ok(())
+use async_std::task;
+
+// #[runtime::main]
+// async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+fn main() -> Result<(), surf::Exception> {
+    task::block_on(async {
+        let res: String = surf::get(url).recv_string().await?;
+        println!("{}", res);
+        Ok::<(), surf::Exception>(())
+    })
 }
+
+// The need for Ok with turbofish is explained here
+// https://rust-lang.github.io/async-book/07_workarounds/03_err_in_async_blocks.html
+// fn main() -> Result<(), surf::Exception> {
+//     // femme::start(log::LevelFilter::Info)?;
+
+//     task::block_on(async {
+//         let uri = "https://httpbin.org/get";
+//         let string: String = surf::get(uri).recv_string().await?;
+//         println!("{}", string);
+//         Ok::<(), surf::Exception>(())
+//     })
+// }
