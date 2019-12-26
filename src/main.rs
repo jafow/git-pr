80211598cacfd9556057c6e170bd2414783d71ca
jafow <jared.a.fowler@gmail.com>
2019-12-25 18:39:40 -0800

    test commit msg
    wip launch editor
    
    comment on regex
    
    move tests
    
    add serde

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
+// #![feature(with_options)]
 
 use std::env;
-use std::fmt::Error;
 use std::fs;
-use std::fs::{File};
-use std::io::{self, Write};
+
+use std::fs::{File, OpenOptions};
+use std::io::{self, prelude::*};
 use std::path::Path;
+use std::process::Command;
 
 use async_std::task;
 use regex::Regex;
 
+// use serde_json::Result;
+
 extern crate clap;
-use clap::{Arg, App, SubCommand};
+use clap::{App, Arg};
 
-
-fn branch_err() -> Error {
-    Error
-}
+const PR_EDITMSG_PATH: &str = ".git/PR_EDITMSG";
 
 fn head_file(head_file: &Path) -> Result<String, io::Error> {
     fs::read_to_string(head_file)
@@ -24,21 +25,28 @@ fn head_file(head_file: &Path) -> Result<String, io::Error> {
 /// read over the .git/HEAD file to get current branch
 fn current_branch(head_file_contents: String) -> Option<String> {
     match head_file_contents.lines().next() {
-        Some(line) => line.split('/').map(String::from).collect::<Vec<String>>().pop(),
-        _ => None
+        Some(line) => line
+            .split('/')
+            .map(String::from)
+            .collect::<Vec<String>>()
+            .pop(),
+        _ => None,
     }
 }
 
-#[test]
-fn test_current_branch() {
-    let hf = head_file(&Path::new("./tests/HEAD_A")).expect("test file");
-    let actual = current_branch(hf);
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_current_branch() {
+        let hf = head_file(&Path::new("./tests/HEAD_A")).expect("test file");
+        let actual = current_branch(hf);
 
-    assert_eq!(Some(String::from("test-branch")), actual)
+        assert_eq!(Some(String::from("test-branch")), actual)
+    }
 }
 
-
-fn get_remote(text: &str) -> Result<Vec<&str>, ()>  {
+fn get_remote(text: &str) -> Result<Vec<&str>, ()> {
+    // captures author, repo, and remote url from git config file
     let re = Regex::new(r#"\[remote\s+"(?P<origin>\w+)"\]\n\turl\s=\s(https?://|git@)github.com[:/]?(?P<author>[A-Za-z0-9_]+)/(?P<repo>[A-Za-z0-9_])"#).unwrap();
 
     let mut v: Vec<&str> = Vec::new();
@@ -46,22 +54,56 @@ fn get_remote(text: &str) -> Result<Vec<&str>, ()>  {
     for caps in re.captures_iter(text) {
         match &caps.name("author") {
             Some(m) => v.push(m.as_str()),
-            None => ()
+            None => (),
         }
 
         match &caps.name("repo") {
             Some(m) => v.push(m.as_str()),
-            None => ()
+            None => (),
         }
 
         match &caps.name("origin") {
             Some(m) => v.push(m.as_str()),
-            None => ()
+            None => (),
         }
-
     }
 
     Ok(v)
+}
+
+fn launch_editor() -> std::io::Result<()> {
+    let editor = env::var("GIT_EDITOR").expect("no $GIT_EDITOR set");
+    let sub = format!("{} ", editor);
+    let cmd = Command::new("sh")
+                .args(&["-c", &sub])
+                .spawn()
+                .and_then(|mut c| c.wait())
+                .expect("error opening editor");
+
+    // let ecode = cmd.wait().expect("open editor failed");
+
+    // dbg!(output);
+    dbg!(cmd);
+    Ok(())
+}
+
+
+fn build_message(target: &str, current: &str) -> std::io::Result<()> {
+    let mut pr_file = File::create(PR_EDITMSG_PATH)?;
+
+    let msg = format!("
+
+// Requesting a pull to {} from {}
+// Write a message for this pull request. The first line
+// of text is the title and the rest is the description.
+// All lines beginning with // will be ignored", target, current);
+
+    pr_file.write_all(msg.as_bytes()).expect("write pr file");
+    Ok(())
+}
+
+fn build_request(target: &str, current: &str, token: String) {
+
 }
 
 fn main() -> std::io::Result<()> {
@@ -69,8 +111,7 @@ fn main() -> std::io::Result<()> {
                     .version("0.1.0")
                     .author("Jared Fowler <jaredafowler@gmail.com>")
                     .about("Open github pull requests")
-                    .usage("git pr origin master
-    git pr upstream feat/add-feature -m \"This is the title of my PR\"")
+                    .usage("git pr origin master\n    git pr upstream feat/add-feature -m \"This is the title of my PR\"")
                     .arg(
                         Arg::with_name("remote")
                         .help("the name of the remote; e.g origin")
@@ -95,40 +136,32 @@ fn main() -> std::io::Result<()> {
     let remote = matches.value_of("remote").unwrap();
     let target = match matches.value_of("target") {
         Some(t) => t,
-        None => "master"
+        None => "master",
     };
 
-    dbg!(remote);
-    dbg!(target);
-
     let git_head = head_file(&Path::new("./.git/HEAD")).expect("git HEAD");
-    let br = current_branch(git_head);
+    let br = current_branch(git_head).unwrap();
     let token = env::var("GITHUB_TOKEN").expect("required GITHUB_TOKEN");
 
-    dbg!(token);
-    dbg!(br);
+    build_message(target, &br)?;
+    launch_editor().expect("launch editor");
+    // build_request(target, &br, token);
+    task::block_on(async {
+        let ff = fetch_api("jafow", &token, "git-pr").await;
+        match ff {
+            Ok(r) => r,
+            Err(e) => panic!("Error {}", e)
+        }
+    });
+
 
     Ok(())
 }
 
-fn fetch_api(uname: &str, password: &str, repo: &str) -> Result<(), surf::Exception> {
-    task::block_on(async {
-        let url = format!("https://{}:{}@api.github.com/repos/{}/{}/pulls?sort=created", &uname, &password, &uname, &repo);
-        dbg!(&url);
-        let res: String = surf::get(url).recv_string().await?;
-        println!("{}", res);
-        Ok::<(), surf::Exception>(())
-    })
-}
-// The need for Ok with turbofish is explained here
-// https://rust-lang.github.io/async-book/07_workarounds/03_err_in_async_blocks.html
-// fn main() -> Result<(), surf::Exception> {
-//     // femme::start(log::LevelFilter::Info)?;
+async fn fetch_api(uname: &str, password: &str, repo: &str) -> Result<surf::Response, Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let url = format!("https://{}:{}@api.github.com/repos/{}/{}/pulls", &uname, &password, &uname, &repo);
 
-//     task::block_on(async {
-//         let uri = "https://httpbin.org/get";
-//         let string: String = surf::get(uri).recv_string().await?;
-//         println!("{}", string);
-//         Ok::<(), surf::Exception>(())
-//     })
-// }
+    let body = serde_json::json!({"title": "foo 3", "head": "feat", "base": "master", "body": "from serde_json"});
+    let res = surf::post(&url).body_json(&body)?.await?;
+    Ok(res)
+}
